ed768316541845dd6f62e05d41d993b602f21816
ManuelMejia <maalmer@gmail.com>
2024-07-07 20:14:12 -0700

    Added script to load toolboxes for Matlab code

--- code/copy_SPM_files.m
+++ code/copy_SPM_files.m
@@ -1,9 +1,9 @@
 function copy_SPM_files(dir_data)
 % datalad run
 % datalad run -m "Copy SPM files" matlab -batch "addpath('code/'); copy_SPM_files('C:\Users\cthul\Dropbox\PROYECTOS\0-PhD\expBA\')"
-
-%     addpath(code);
 %     dir_data = 'C:\Users\cthul\Dropbox\PROYECTOS\0-PhD\expBA\';
+
+    load_toolboxes;
     dir_out = 'data\';
 
     dir_list = spm_select('List', dir_data, 'dir', '(sub-S|sub-EP)');
@@ -11,6 +11,7 @@ function copy_SPM_files(dir_data)
     file_to_copy = fullfile(dir_data, deblank(dir_list(1,:)), 'SPM.mat');
     file_to_save = fullfile(dir_out, deblank(dir_list(1,:)), 'SPM.mat');
 
+    fprintf('\n ... Copying file to: %s\n', file_to_save);
     copyfile(file_to_copy, file_to_save)
 
 end
---
+++ code/load_toolboxes.m
@@ -0,0 +1,3 @@
+toolbox_path = 'C:\Users\cthul\Dropbox\PROYECTOS\0-PhD\MLtoolboxes\';
+
+addpath(fullfile(toolbox_path, 'spm12'));
