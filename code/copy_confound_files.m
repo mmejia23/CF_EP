95d7fc1ca64a208f1097f67bf6f60767ea21e36f
ManuelMejia <maalmer@gmail.com>
2024-07-10 23:09:22 -0700

    Added script to copy confound files of participants from fmriprep output

--- code/copy_confound_files.m
+++ code/copy_confound_files.m
@@ -9,12 +9,13 @@ function copy_confound_files(dir_data)
     dir_list = spm_select('List', dir_data, 'dir', '(sub-S|sub-EP)');
 
     for i = 1:size(dir_list,1)
+        dir_sub = deblank(dir_list(i,:));
         tsv_file_to_copy = spm_select('FPListRec', dir_sub, 'desc-confounds_timeseries.tsv');
         [~,NAME,EXT] = fileparts(tsv_file_to_copy);
-        tsv_file_to_save = fullfile(dir_out, deblank(dir_list(i,:)), 'func', [NAME, EXT]);
+        tsv_file_to_save = fullfile(dir_out, dir_sub, 'func', [NAME, EXT]);
         json_file_to_copy = spm_select('FPListRec', dir_sub, 'desc-confounds_timeseries.json');
         [~,NAME,EXT] = fileparts(json_file_to_copy);
-        json_file_to_save = fullfile(dir_out, deblank(dir_list(i,:)), 'func', [NAME, EXT]);
+        json_file_to_save = fullfile(dir_out, dir_sub, 'func', [NAME, EXT]);
 
         fprintf('\n ... Copying file to: %s\n', tsv_file_to_save);
         copyfile(tsv_file_to_copy, tsv_file_to_save)
