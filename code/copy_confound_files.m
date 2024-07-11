45dc36b9733d4e690cef51ba18bc5e147ff27465
ManuelMejia <maalmer@gmail.com>
2024-07-10 23:13:19 -0700

    Fixed absolute path within copy_confound_files function

--- code/copy_confound_files.m
+++ code/copy_confound_files.m
@@ -10,10 +10,10 @@ function copy_confound_files(dir_data)
 
     for i = 1:size(dir_list,1)
         dir_sub = deblank(dir_list(i,:));
-        tsv_file_to_copy = spm_select('FPListRec', dir_sub, 'desc-confounds_timeseries.tsv');
+        tsv_file_to_copy = spm_select('FPListRec', fullfile(dir_data, dir_sub), 'desc-confounds_timeseries.tsv');
         [~,NAME,EXT] = fileparts(tsv_file_to_copy);
         tsv_file_to_save = fullfile(dir_out, dir_sub, 'func', [NAME, EXT]);
-        json_file_to_copy = spm_select('FPListRec', dir_sub, 'desc-confounds_timeseries.json');
+        json_file_to_copy = spm_select('FPListRec', fullfile(dir_data, dir_sub), 'desc-confounds_timeseries.json');
         [~,NAME,EXT] = fileparts(json_file_to_copy);
         json_file_to_save = fullfile(dir_out, dir_sub, 'func', [NAME, EXT]);
 
